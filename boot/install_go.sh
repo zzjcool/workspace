fbad833001c2f21e734b868fc18560c05367c04f
zhijiezheng <zhijiezheng@tencent.com>
2022-01-19 11:46:40 +0800

    fix: 修正环境变量问题

--- boot/install_go.sh
+++ boot/install_go.sh
@@ -6,7 +6,7 @@ GO_VERSION="1.17.5"
 PROTOC_VERSION="3.19.1"
 wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
 tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.rc
+echo "export PATH=\$PATH:/usr/local/go/bin:/root/go/bin" >> ~/.rc
 wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip
 unzip protoc-${PROTOC_VERSION}-linux-x86_64.zip
 mv bin/protoc /usr/local/bin/
